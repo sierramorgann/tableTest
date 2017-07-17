7d11708513592f7a25496b13aa1ec935f74d8bdb
Sierra Morgan <you@example.com>
2017-07-17 11:39:49 -0400

    first commit

---
+++ TableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableTest
+//
+//  Created by Sierra Morgan on 7/17/17.
+//  Copyright © 2017 Sierra Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
